0426c7ead07ab990c2694d6b0c97db94daa6307d
Edward <108880063+kpboomy@users.noreply.github.com>
2023-01-17 12:48:07 -0500

    Express Router
    intro to express

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
-const http = require('http');
+const express = require('express');
+const morgan = require('morgan');
+const campsiteRouter = require('./routes/campsiteRouter');
 
 const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    console.log(req.headers);
+const app = express();
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/campsites', campsiteRouter);
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Hello World!</h1></body></html>');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 });
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
